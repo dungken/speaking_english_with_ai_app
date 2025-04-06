b5dab50b1fa602ab88d76b9ec30197af34b1a7a6
Nhật Nguyễn <minhnhatzzsi4@gmail.com>
2025-04-06 12:58:12 +0700

    Add middleware for allowing sending request

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from app.routes import user, conversation
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Configure CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 app.include_router(user.router, prefix="/api/users", tags=["users"])
 app.include_router(conversation.router, prefix="/api", tags=["conversations"])
